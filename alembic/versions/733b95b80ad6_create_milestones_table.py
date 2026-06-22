f124ffc0e3c91994cb34cab1acb6dda5b60ffd82
Katrin Torsunova <to.katrin.t@gmail.com>
2026-06-22 21:59:51 +0200

    Fix CC

--- alembic/versions/733b95b80ad6_create_milestones_table.py
+++ alembic/versions/733b95b80ad6_create_milestones_table.py
@@ -1,12 +1,12 @@
 """create milestones table"""
+import sqlalchemy as sa
+from alembic import op
+
 
 revision = '733b95b80ad6'
 down_revision = None
 branch_labels = None
 depends_on = None
-
-from alembic import op
-import sqlalchemy as sa
 
 
 def upgrade() -> None:
