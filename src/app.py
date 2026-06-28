62aa16ab8057010fa77d9b36fe651097802137fd
Katrin Torsunova <to.katrin.t@gmail.com>
2026-06-28 13:43:30 +0200

    disable fastapi docs in production app

--- src/app.py
+++ src/app.py
@@ -22,7 +22,12 @@ async def lifespan(app: FastAPI):
     yield
 
 
-app = FastAPI(lifespan=lifespan)
+app = FastAPI(
+    lifespan=lifespan,
+    docs_url=None,
+    redoc_url=None,
+    openapi_url=None,
+)
 app.add_middleware(AuthMiddleware)
 app.include_router(auth_router)
 app.mount("/static", StaticFiles(directory=SRC / "static"), name="static")
